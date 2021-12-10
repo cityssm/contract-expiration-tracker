d273af2e0a91aad8de19e3b7bb25b01826e13bf2
dangowans <d.gowans@cityssm.on.ca>
2021-12-10 15:11:57 -0500

    add admin handlers to router

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import handler_doGetContractCategoryUsers from "../handlers/doGetContractCategoryUsers.js";
+import handler_doAddContractCategoryUser from "../handlers/doAddContractCategoryUser.js";
+import handler_doRemoveContractCategoryUser from "../handlers/doRemoveContractCategoryUser.js";
+import handler_doGetUserNames from "../handlers/doGetUserNames.js";
 export const router = Router();
-router.get("/doGetContactCategoryUsers", handler_doGetContractCategoryUsers);
+router.post("/doGetContractCategoryUsers", handler_doGetContractCategoryUsers);
+router.post("/doAddContractCategoryUser", handler_doAddContractCategoryUser);
+router.post("/doRemoveContractCategoryUser", handler_doRemoveContractCategoryUser);
+router.post("/doGetUserNames", handler_doGetUserNames);
 export default router;
