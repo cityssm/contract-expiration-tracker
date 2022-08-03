d5ff9b721f6b3ac0710656aa1bea7c218d609ae5
Dan Gowans <d.gowans@cityssm.on.ca>
2022-08-03 08:59:52 -0400

    build stylesheets with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,36 @@
 import gulp from "gulp";
+import changed from "gulp-changed";
 import minify from "gulp-minify";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+const publicSCSSDestination = "public/stylesheets";
+const publicSCSSFunction = () => {
+    return gulp.src("public-scss/*.scss")
+        .pipe(sass({
+        outputStyle: 'compressed',
+        includePaths: ['./', './node_modules']
+    }).on('error', sass.logError))
+        .pipe(gulp.dest(publicSCSSDestination));
+};
+gulp.task("public-scss", publicSCSSFunction);
+const publicJavascriptsDestination = "public/javascripts";
 const publicJavascriptsMinFunction = () => {
     return gulp.src("public-typescript/*.js", { allowEmpty: true })
+        .pipe(changed(publicJavascriptsDestination, {
+        extension: ".min.js"
+    }))
         .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
-        .pipe(gulp.dest("public/javascripts"));
+        .pipe(gulp.dest(publicJavascriptsDestination));
 };
 gulp.task("public-javascript-min", publicJavascriptsMinFunction);
 const watchFunction = () => {
+    gulp.watch("public-scss/*.scss", publicSCSSFunction);
     gulp.watch("public-typescript/*.js", publicJavascriptsMinFunction);
 };
 gulp.task("watch", watchFunction);
 gulp.task("default", () => {
     publicJavascriptsMinFunction();
+    publicSCSSFunction();
     watchFunction();
 });
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,17 +1,45 @@
 /* eslint-disable node/no-unpublished-import */
 
 import gulp from "gulp";
+import changed from "gulp-changed";
 import minify from "gulp-minify";
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+/*
+ * Compile SASS
+ */
+
+const publicSCSSDestination = "public/stylesheets";
+
+const publicSCSSFunction = () => {
+
+  return gulp.src("public-scss/*.scss")
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: ['./', './node_modules']
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(publicSCSSDestination));
+};
+
+gulp.task("public-scss", publicSCSSFunction);
 
 /*
  * Minify public/javascripts
  */
 
+const publicJavascriptsDestination = "public/javascripts";
+
 const publicJavascriptsMinFunction = () => {
 
   return gulp.src("public-typescript/*.js", { allowEmpty: true })
+    .pipe(changed(publicJavascriptsDestination, {
+      extension: ".min.js"
+    }))
     .pipe(minify({ noSource: true, ext: { min: ".min.js" } }))
-    .pipe(gulp.dest("public/javascripts"));
+    .pipe(gulp.dest(publicJavascriptsDestination));
 };
 
 
@@ -22,6 +50,7 @@ gulp.task("public-javascript-min", publicJavascriptsMinFunction);
  */
 
 const watchFunction = () => {
+  gulp.watch("public-scss/*.scss", publicSCSSFunction);
   gulp.watch("public-typescript/*.js", publicJavascriptsMinFunction);
 };
 
@@ -33,5 +62,6 @@ gulp.task("watch", watchFunction);
 
 gulp.task("default", () => {
   publicJavascriptsMinFunction();
+  publicSCSSFunction();
   watchFunction();
 });
